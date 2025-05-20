54d3af09a721fce14efe80f24b886726b5ea4fcc
Claudio Arch <cjramos96@protonmail.com>
2025-05-19 23:19:09 -0300

    FIX: fixed the error, now the project works

--- Mapper/ExampleClass/ClientEntity.cs
+++ Mapper/ExampleClass/ClientEntity.cs
@@ -2,7 +2,7 @@ namespace Mapper.ExampleClass;
 
 public class ClientEntity
 {
-    public Guid Id { get; set; }
+    public string? Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
