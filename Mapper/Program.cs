596d20cadc732ef336bc39e961fa093856d0e2a9
Claudio Arch <cjramos96@protonmail.com>
2025-05-19 22:52:00 -0300

    First commit

---
+++ Mapper/Program.cs
@@ -0,0 +1,21 @@
+﻿using Mapper;
+using Mapper.ExampleClass;
+
+var map = new Mapping();
+
+var clienteDto = new ClientDTO()
+{
+    Name = "TesteNome",
+    Email = "Email@email.com",
+    Password = "Password",
+    Phone = "123456789"
+};
+
+map.Map<ClientEntity>(clienteDto);
+
+// var retorno = map.Map<ClientEntity>(clienteDto);
+//
+// Console.WriteLine($"Nome: {retorno.Name}");
+// Console.WriteLine($"Email: {retorno.Email}");
+// Console.WriteLine($"Password: {retorno.Password}");
+// Console.WriteLine($"Telefone: {retorno.Phone}");
